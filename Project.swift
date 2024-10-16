a457f6c32b4d3196b0d149d067beb122606d71eb
40322624 <jdelaney07@qub.ac.uk>
2024-10-16 12:55:42 +0000

    Setting up Project

---
+++ Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "Project301",
+    targets: [
+        .target(
+            name: "Project301",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "co.jackdelaney.p301",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["project-p301/Sources/**"],
+            resources: ["project-p301/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "Project301Tests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "co.jackdelaney.p301Tests",
+            infoPlist: .default,
+            sources: ["project-p301/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "Project301")]
+        ),
+    ]
+)
---
+++ project-p301/Sources/ProjectP301App.swift
@@ -0,0 +1,17 @@
+//
+//  ContentsView.swift
+//  Project301
+//
+//  Created by Jack Delaney on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjectP301App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
