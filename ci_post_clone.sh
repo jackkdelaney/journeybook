9579e642f1cf106331647fb240db7891e6cd8c64
Jack Delaney <jdelaney07@qub.ac.uk>
2024-11-01 22:30:52 +0000

    fix

--- ci_post_clone.sh
+++ ci_post_clone.sh
@@ -4,8 +4,3 @@ mise install # Installs the version from .mise.toml
 
 # Runs the version of Tuist indicated in the .mise.toml file {#runs-the-version-of-tuist-indicated-in-the-misetoml-file}
 mise x tuist generate
-
-if [ ! -d "JourneyBook.xcworkspacee" ]; then
-    echo "Error: JourneyBook.xcworkspacee was not generated."
-    exit 1
-fi
