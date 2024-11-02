ba340937e74b2bd3c0a79977e8027e188dcea85f
Jack Delaney <jdelaney07@qub.ac.uk>
2024-11-02 12:51:17 +0000

    Mise Install

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,7 +6,7 @@
 
 
 curl https://mise.jdx.dev/install.sh | sh
-# mise install # Installs the version from .mise.toml
+mise install # Installs the version from .mise.toml
 
 # # Runs the version of Tuist indicated in the .mise.toml file {#runs-the-version-of-tuist-indicated-in-the-misetoml-file}
 # mise x tuist generate
