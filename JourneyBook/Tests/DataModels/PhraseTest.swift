ab26e21fe02399a87eaf3d10fb9c88c9537d100d
Jack Delaney <jdelaney07@qub.ac.uk>
2025-01-01 13:09:13 +0000

    Adds Extra Tests

---
+++ JourneyBook/Tests/DataModels/PhraseTest.swift
@@ -0,0 +1,49 @@
+import Testing
+import Foundation
+
+@testable import JourneyBook
+
+struct PhraseTest{
+    
+    @Test
+    func testInitialisationWithJustPhraseText() {
+        let phrase = Phrase(text: "Hello World")
+        #expect(phrase.text == "Hello World")
+        #expect(phrase.id != UUID())
+        #expect(phrase.dateCreated.timeIntervalSinceNow < 1)
+        #expect(phrase.dateModified.timeIntervalSinceNow < 1)
+    }
+    
+    @Test
+    func testInitialisationWithJustPhraseTextAndID() {
+        let id = UUID()
+        let phrase = Phrase(text: "Hello World", id: id)
+        #expect(phrase.text == "Hello World")
+        #expect(phrase.id == id)
+        #expect(phrase.dateCreated.timeIntervalSinceNow < 1)
+        #expect(phrase.dateModified.timeIntervalSinceNow < 1)
+    }
+    
+    @Test
+    func testInitialisationAllArguments() {
+        let id = UUID()
+        let dateCreated = Date.distantPast
+        let dateModified = Date.distantFuture
+        let phrase = Phrase(text: "Hello World", id: id,dateCreated:dateCreated,dateModified: dateModified)
+        #expect(phrase.text == "Hello World")
+        #expect(phrase.id == id)
+        #expect(phrase.dateCreated == dateCreated)
+        #expect(phrase.dateModified == dateModified)
+    }
+    
+    @Test
+    func testTextMutationUpdatesDateModified() {
+        var phrase = Phrase(text: "Initial Text")
+        let initialModifiedDate = phrase.dateModified
+        phrase.text = "Updated Text"
+        
+        #expect(phrase.text == "Updated Text")
+        #expect(phrase.dateModified > initialModifiedDate)
+    }
+
+}
