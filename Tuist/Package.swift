938673da19c7764a7e49824691a6e3891d557800
Jack Delaney <jdelaney07@qub.ac.uk>
2025-01-05 01:04:58 +0000

    Added Feedkit

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -8,13 +8,14 @@ import PackageDescription
         // Customize the product types for specific package product
         // Default is .staticFramework
         // productTypes: ["Alamofire": .framework,]
-        productTypes: [:]
+        productTypes: ["FeedKit": .framework,]
     )
 #endif
 
 let package = Package(
     name: "JourneyBook",
     dependencies: [
+        .package(url: "https://github.com/nmdias/FeedKit", from: "9.1.2")
         // Add your own dependencies here:
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
