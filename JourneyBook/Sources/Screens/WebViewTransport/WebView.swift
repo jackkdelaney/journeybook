1b7448d3fa09b2fd677ce1bf44e7d2cca0bdecb6
Jack Delaney <jdelaney07@qub.ac.uk>
2025-01-05 16:40:05 +0000

    Basic Web Support

---
+++ JourneyBook/Sources/Screens/WebViewTransport/WebView.swift
@@ -0,0 +1,61 @@
+//
+//  WebView.swift
+//  JourneyBook
+//
+//  Created by Jack Delaney on 05/01/2025.
+//
+
+import SwiftUI
+@preconcurrency import WebKit
+
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    let onConfirm: (String) -> Void
+    @Binding var canGoBack: Bool
+    @Binding var isNonBustimePage: Bool
+    @Binding var currentURL: String?
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self, canGoBack: $canGoBack, isNonBustimePage: $isNonBustimePage, currentURL: $currentURL, onConfirm: onConfirm)
+    }
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        webView.uiDelegate = context.coordinator
+        let request = URLRequest(url: url)
+        webView.load(request)
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+
+    class Coordinator: NSObject, WKNavigationDelegate, WKUIDelegate {
+        var parent: WebView
+        @Binding var canGoBack: Bool
+        @Binding var isNonBustimePage: Bool
+        @Binding var currentURL: String?
+        var onConfirm: (String) -> Void
+
+        init(_ parent: WebView, canGoBack: Binding<Bool>, isNonBustimePage: Binding<Bool>, currentURL: Binding<String?>, onConfirm: @escaping (String) -> Void) {
+            self.parent = parent
+            self._canGoBack = canGoBack
+            self._isNonBustimePage = isNonBustimePage
+            self._currentURL = currentURL
+            self.onConfirm = onConfirm
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            canGoBack = webView.canGoBack
+            if let url = webView.url?.absoluteString {
+                isNonBustimePage = !url.contains("bustimes.org")
+                if url.contains("bustimes.org/services/") {
+                    currentURL = url
+                } else {
+                    currentURL = nil
+                }
+            }
+        }
+    }
+}
