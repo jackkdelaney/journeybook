43bcf3d99aeab7610b56aa1b924f2a1e52d3fb79
Jack Delaney <jdelaney07@qub.ac.uk>
2025-01-06 09:15:11 +0000

    DataModel etc

--- JourneyBook/Sources/Screens/TransportRoutes/AddTransportRouteView.swift
+++ JourneyBook/Sources/Screens/TransportRoutes/AddTransportRouteView.swift
@@ -7,17 +7,49 @@
 
 import SwiftUI
 
-struct AddTransportRouteView : SheetView {
-    var confirmButton: some View {
-        Text("CONFIRM")
+struct AddTransportRouteView: SheetView {
+    @State var model = AddTransportRouteViewModel()
+
+    var content: some View {
+        Form {
+            Section("Route Name") {
+                TextField("Route Name", text: model.routeNameBinding)
+            }
+            Section("Route") {
+                if let url = model.url {
+                    Text("\(url)")
+                } else {
+                    Button("Select Transport Route Online") {
+                        
+                    }
+                }
+            }
+        }
     }
-    
+
+    var confirmButton: some View {
+        Button("Save") {
+            model.saveItem()
+        }
+        .disabled(!model.saveable)
+    }
+
     var sheetTitle: String {
         "Add New Route"
     }
-    var content: some View {
-        Form {
-            Text("Content")
-        }
+}
+
+extension AddTransportRouteViewModel {
+    var routeNameBinding: Binding<String> {
+        Binding(
+            get: { self.routeName ?? "" },
+            set: {
+                if $0 == "" {
+                    self.routeName = nil
+                } else {
+                    self.routeName = $0
+                }
+            }
+        )
     }
 }
