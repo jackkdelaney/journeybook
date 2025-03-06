c395532871e793b1ff9a8fbe0c14aa47d30165e2
Jack Delaney <jdelaney07@qub.ac.uk>
2025-03-06 23:44:32 +0000

    improvements

---
+++ JourneyBook/Sources/Screens/JourneyStep/JourneyStepInputForm.swift
@@ -0,0 +1,108 @@
+//
+//  JourneyStepInputForm.swift
+//  JourneyBook
+//
+//  Created by Jack Delaney on 06/03/2025.
+//
+
+import CoreLocation
+import SwiftUI
+
+struct JourneyStepInputForm: View {
+    @State private var sheet: AddJourneyStepSheet?
+
+    @Binding var localName: String
+    @Binding var localDescription: String
+
+    @Binding var cordinates: CLLocationCoordinate2D?
+
+    @Binding var resources: [VisualResource]
+
+    @Binding var publicTransit: TransportRoute?
+
+    @State var phrases: [Phrase]
+
+    var body: some View {
+        Form {
+            Section("Step Name") {
+                TextField("Step Name", text: $localName)
+            }
+            Section("Description") {
+                TextEditor(text: $localDescription)
+            }
+            locationSection
+            resourceSection
+            publicTransitSection
+            phraseSection
+        }
+        .sheet(item: $sheet) { item in
+            item.buildView()
+        }
+    }
+
+    @ViewBuilder
+    private var resourceSection: some View {
+        ForEach(resources) { visualResource in
+            ResourceSection(resource: visualResource)
+        }
+
+        Section("Resource") {
+            Button("Select Resource") {
+                let resourceWrapped = AddJourneyLocationVisualResourceGetter(resources: $resources)
+                sheet = .getVisualResourceFromList(resourceWrapped)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var publicTransitSection: some View {
+        if let publicTransitResource = publicTransit {
+            Section("Public Transit") {
+                Text("\(publicTransitResource.url)")
+                Button("Edit") {
+                    let transportWrapped = AddJourneyTransportGetter(transport: $publicTransit)
+                    sheet = .getTransportRouteFromList(transportWrapped)
+                }
+            }
+        } else {
+            Section("Public Transit") {
+                Button("Add Public Transport Route") {
+                    let transportWrapped = AddJourneyTransportGetter(transport: $publicTransit)
+                    sheet = .getTransportRouteFromList(transportWrapped)
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var phraseSection: some View {
+        Section(phraseText) {
+            Button("Select Phrase's") {
+                let phrasesWrapped = AddJourneyPhraseSelectionGetter(phrases: $phrases)
+                sheet = .selectPhrases(phrasesWrapped)
+            }
+        }
+    }
+
+    private var locationSection: some View {
+        Section("Location") {
+            if let cordinates {
+                MapInDetailView(location: JourneyStepLocation(location: cordinates))
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .removeListRowPaddingInsets()
+            } else {
+                Button("Find Location") {
+                    let locationWrapped = AddJourneyLocationStepGetter(location: $cordinates)
+                    sheet = .getLocationFromAddress(locationWrapped)
+                }
+            }
+        }
+    }
+
+    var phraseText: String {
+        return String(AttributedString(
+            localized: "You have ^[\(phrases.count) \("Phrase")](inflect: true) in this step."
+        ).characters)
+    }
+}
