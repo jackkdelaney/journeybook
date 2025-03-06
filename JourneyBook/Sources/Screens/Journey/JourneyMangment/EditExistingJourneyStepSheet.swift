ceaa0e66e5b8d953ae8774618171c67f49bba94c
Jack Delaney <jdelaney07@qub.ac.uk>
2025-03-06 13:39:39 +0000

    improvrmemnts

---
+++ JourneyBook/Sources/Screens/Journey/JourneyMangment/EditExistingJourneyStepSheet.swift
@@ -0,0 +1,41 @@
+//
+//  EditExistingJourneyStepSheet.swift
+//  JourneyBook
+//
+//  Created by Jack Delaney on 06/03/2025.
+//
+
+import Foundation
+import SwiftUI
+
+struct EditExistingJourneyStepSheet: SheetView {
+    @Bindable var journeyStep: JourneyStep
+
+    var sheetTitle: String {
+        "Edit Step"
+    }
+
+    var content: some View {
+        Form {
+            Text("BOB")
+        }
+    }
+
+    var confirmButton: some View {
+        Button("Update") {
+//            do {
+//                if journeyName.isEmpty {
+//                    throw JourneyViewModelError.noJourneyText
+//                }
+//                journey.journeyName = journeyName
+//                journey.journeyDescription = journeyDescription
+//                try modelContext.save()
+//                dismiss()
+//            } catch JourneyViewModelError.noJourneyText {
+//                errorMessage = .noJourneyText
+//            } catch {
+//                print(error)
+//            }
+        }
+    }
+}
