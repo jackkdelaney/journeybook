3b223631017c2911115da263da45f247f057984b
Jack Delaney <jdelaney07@qub.ac.uk>
2025-03-11 09:18:39 +0000

    ADD searchbar

--- JourneyBook/Sources/Screens/Journey/JourneyItemsView.swift
+++ JourneyBook/Sources/Screens/Journey/JourneyItemsView.swift
@@ -16,48 +16,44 @@ struct JourneyItemsView: View {
 
     @Binding var sheet: JourneySheet?
 
+    @Binding var searchText: String
+
     @ViewBuilder
     var body: some View {
+        if searchText.isEmpty {
+            bodyStandard
+        } else {
+            bodySearch
+        }
+    }
+
+    @ViewBuilder
+    var bodySearch: some View {
+        if !filteredBySearchJourneys.isEmpty {
+            Section("Journey's for \(searchText)") {
+                ForEach(filteredBySearchJourneys) { journey in
+                    journeyButton(for: journey)
+                }
+                .onDelete(perform: deleteSearch)
+            }
+        } else {
+            ContentUnavailableView.search
+        }
+    }
+
+    @ViewBuilder
+    var bodyStandard: some View {
         if !journeys.isEmpty {
             Section("Journey's") {
                 ForEach(journeys) { journey in
-                    Button {
-                        coordinator.push(page: .journeyDetails(journey))
-                    } label: {
-                        VStack(alignment: .leading) {
-                            Text("\(journey.journeyName)")
-                                .font(.headline)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            Text(journey.dateCreated.formatted())
-                                .font(.subheadline)
-                                .foregroundStyle(.secondary)
-                                .lineLimit(1)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-
-                            if let description = journey.journeyDescription {
-                                Text(description)
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
-                                    .lineLimit(2)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                            }
-                        }
-                    }
-                    .chevronButtonStyle(compact: true)
-                    .contextMenu {
-                        Button {
-                            sheet = .editJourney(journey)
-                        } label: {
-                            Label("Edit", systemImage: "pencil")
-                        }
-                    }
+                    journeyButton(for: journey)
                 }
                 .onDelete(perform: delete)
             }
         }
     }
 
-    func delete(at offsets: IndexSet) {
+    private func delete(at offsets: IndexSet) {
         for offset in offsets {
             let journey = journeys[offset]
             for step in journey.steps {
@@ -68,5 +64,58 @@ struct JourneyItemsView: View {
         do {
             try modelContext.save()
         } catch {}
+    }
+
+    private func deleteSearch(at offsets: IndexSet) {
+        for offset in offsets {
+            let journey = filteredBySearchJourneys[offset]
+            for step in journey.steps {
+                modelContext.delete(step)
+            }
+            modelContext.delete(journey)
+        }
+        do {
+            try modelContext.save()
+        } catch {}
+    }
+
+    @ViewBuilder
+    private func journeyButton(for journey: Journey) -> some View {
+        Button {
+            coordinator.push(page: .journeyDetails(journey))
+        } label: {
+            VStack(alignment: .leading) {
+                Text("\(journey.journeyName)")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(journey.dateCreated.formatted())
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                    .lineLimit(1)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                if let description = journey.journeyDescription {
+                    Text(description)
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                        .lineLimit(2)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+            }
+        }
+        .chevronButtonStyle(compact: true)
+        .contextMenu {
+            Button {
+                sheet = .editJourney(journey)
+            } label: {
+                Label("Edit", systemImage: "pencil")
+            }
+        }
+    }
+
+    private var filteredBySearchJourneys: [Journey] {
+        journeys.filter { journey in
+            journey.journeyName.contains(searchText)
+        }
     }
 }
