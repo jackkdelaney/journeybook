01d511fe82ea0bcb07322968cc7a5bd6404b5142
Jack Delaney <jdelaney07@qub.ac.uk>
2025-04-04 10:43:57 +0100

    IMPROVEMENTS

---
+++ JourneyBook/Sources/SiriAndShortcuts/AppShortcuts.swift
@@ -0,0 +1,65 @@
+//
+//  AppShortcuts.swift
+//  JourneyBook
+//
+//  Created by Jack Delaney on 03/04/2025.
+//
+
+import AppIntents
+import SwiftData
+
+class AppShortcuts: AppShortcutsProvider {
+    static var appShortcuts: [AppShortcut] {
+//        AppShortcut(
+//            intent: NewWishIntent(),
+//            phrases: [
+//                "create new wish in \(.applicationName)",
+//                "using \(.applicationName) create a new wish"
+//            ],
+//            shortTitle: "Create Wishlist Item",
+//            systemImageName: "sparkles"
+//        )
+//        
+        AppShortcut(
+            intent: OpenJourneyIntent(),
+            phrases: [
+                "open \(\.$wish) using \(.applicationName)"
+            ],
+            shortTitle: "Open Wish",
+            systemImageName: "sparkles.square.filled.on.square"
+        )
+        
+//        AppShortcut(
+//            intent: UpdateWishStatusIntent(),
+//            phrases: [
+//                "update status of \(\.$wish) using \(.applicationName)"
+//            ],
+//            shortTitle: "Update Status",
+//            systemImageName: "wand.and.stars.inverse"
+//        )
+
+    }
+}
+
+
+enum ModelContextProvider {
+    static var context: ModelContext?
+}
+
+extension ModelContext {
+    static func getContextForAppIntents() -> ModelContext {
+        if let context = ModelContextProvider.context {
+            return context
+        }
+        
+        let schema = Schema([VisualResource.self, Phrase.self, Journey.self, JourneyStep.self, TransportRoute.self, Communication.self])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            let container = try ModelContainer(for: schema, configurations: [modelConfiguration])
+            return .init(container)
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+}
--- JourneyBook/Sources/SiriAndShortcuts/PickJourneyIntent.swift
+++ JourneyBook/Sources/SiriAndShortcuts/PickJourneyIntent.swift
@@ -5,6 +5,8 @@
 //  Created by Jack Delaney on 20/03/2025.
 //
 
+
+import Foundation
 import AppIntents
 import SwiftData
 
@@ -12,76 +14,33 @@ import SwiftData
 
 
 
+struct OpenJourneyIntent: AppIntent {
+    static var title: LocalizedStringResource = "Open Journey"
+    static var openAppWhenRun = true
+    
+    @Parameter(title: "Journey")
+    var wish: JourneyEntiy
+    
+    static var parameterSummary: some ParameterSummary {
+        Summary("Open \(\.$wish)")
+    }
+    
+    func perform() async throws -> some IntentResult & ProvidesDialog {
+        let modelContext = ModelContext.getContextForAppIntents()
+        guard let navigationCoordinator = AppNavigationCoordinator.activeCoordinator else {
+            throw IntentError.coordinatorNotFound
+        }
+        
+        let fetchDescriptor = FetchDescriptor<WishlistItem>()
+        let items = try? modelContext.fetch(fetchDescriptor)
+        guard let swiftDataItem = (items?.first { $0.id == wish.id }) else {
+            throw IntentError.itemNotFound
+        }
+        
+        navigationCoordinator.clear()
+        navigationCoordinator.navigate(to: swiftDataItem)
+        
+        return .result(dialog: "Opening...")
+    }
+}
 
-//struct JourneyEntity: AppEntity {
-//    static var typeDisplayRepresentation: TypeDisplayRepresentation = "Wishlist Item"
-//
-//    static var defaultQuery = JourneyEntityQuery()
-//
-//    let id: UUID
-//
-//    @Property(title: "Wishlist Item Title")
-//    var title: String
-//
-//    var displayRepresentation: DisplayRepresentation {
-//        DisplayRepresentation(
-//            title: "\(title)"
-//        )
-//    }
-//
-//    init(swiftDataModel: Journey) {
-//        self.id = swiftDataModel.id
-//        self.title = swiftDataModel.journeyName
-//    }
-//}
-//
-//
-//struct JourneyEntityQuery: EntityQuery {
-//    func entities(for identifiers: [JourneyEntity.ID]) async throws -> [JourneyEntity] {
-//        let modelContext = ModelContext.getContextForAppIntents()
-//        let fetchDescriptor = FetchDescriptor<Journey>(
-//            predicate: #Predicate { identifiers.contains($0.id) }
-//        )
-//        
-//        return try modelContext
-//            .fetch(fetchDescriptor)
-//            .map(JourneyEntity.init)
-//    }
-//    
-//    
-//    func suggestedEntities() async throws -> [JourneyEntity] {
-//        let modelContext = ModelContext.getContextForAppIntents()
-//        let fetchDescriptor = FetchDescriptor<Journey>()
-//        
-//        return try modelContext
-//            .fetch(fetchDescriptor)
-//            .map(JourneyEntity.init)
-//    }
-//}
-//
-//
-//enum ModelContextProvider {
-//    static var context: ModelContext?
-//}
-//
-//
-//extension ModelContext {
-//    static func getContextForAppIntents() -> ModelContext {
-//        if let context = ModelContextProvider.context {
-//            return context
-//        }
-//        
-//        let schema = Schema([
-//            VisualResource.self, Phrase.self, Journey.self,
-//            JourneyStep.self, TransportRoute.self, Communication.self,
-//        ])
-//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-//
-//        do {
-//            let container = try ModelContainer(for: schema, configurations: [modelConfiguration])
-//            return .init(container)
-//        } catch {
-//            fatalError("Could not create ModelContainer: \(error)")
-//        }
-//    }
-//}
