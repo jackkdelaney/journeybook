d4b31f090e64d7ce2744813dd776b7deb24f2158
Jack Delaney <jdelaney07@qub.ac.uk>
2025-04-04 10:43:57 +0100

    IMPROVEMENTS

---
+++ JourneyBook/Sources/SiriAndShortcuts/JourneyEntiy.swift
@@ -0,0 +1,55 @@
+//
+//  JourneyEntiy.swift
+//  JourneyBook
+//
+//  Created by Jack Delaney on 04/04/2025.
+//
+
+import Foundation
+import AppIntents
+import SwiftData
+
+struct JourneyEntiy: AppEntity {
+    static var typeDisplayRepresentation: TypeDisplayRepresentation = "Journey Item"
+    
+    static var defaultQuery = JourneyEntiyQuery()
+
+    let id: UUID
+    
+    @Property(title: "Journey Item Title")
+    var journeyName: String
+    
+    var displayRepresentation: DisplayRepresentation {
+        DisplayRepresentation(
+            title: "\(journeyName)"
+        )
+    }
+    
+    init(swiftDataModel: Journey) {
+        self.id = swiftDataModel.id
+        self.journeyName = swiftDataModel.journeyName
+    }
+}
+
+struct JourneyEntiyQuery: EntityQuery {
+    func entities(for identifiers: [JourneyEntiy.ID]) async throws -> [JourneyEntiy] {
+        let modelContext = ModelContext.getContextForAppIntents()
+        let fetchDescriptor = FetchDescriptor<Journey>(
+            predicate: #Predicate { identifiers.contains($0.id) }
+        )
+        
+        return try modelContext
+            .fetch(fetchDescriptor)
+            .map(JourneyEntiy.init)
+    }
+    
+    
+    func suggestedEntities() async throws -> [JourneyEntiy] {
+        let modelContext = ModelContext.getContextForAppIntents()
+        let fetchDescriptor = FetchDescriptor<Journey>()
+        
+        return try modelContext
+            .fetch(fetchDescriptor)
+            .map(JourneyEntiy.init)
+    }
+}
