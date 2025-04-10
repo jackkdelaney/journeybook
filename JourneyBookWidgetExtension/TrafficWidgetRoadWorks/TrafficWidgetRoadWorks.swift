39733f2ca4fd10de475ae3384b06ad262007553b
Jack Delaney <jdelaney07@qub.ac.uk>
2025-04-11 00:03:29 +0100

    Improvemnts

---
+++ JourneyBookWidgetExtension/TrafficWidgetRoadWorks/TrafficWidgetRoadWorks.swift
@@ -0,0 +1,32 @@
+//
+//  TrafficWidgetRoadWorks.swift
+//  JourneyBookWidgetExtension
+//
+//  Created by Jack Delaney on 10/04/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct TrafficWidgetRoadWorks: Widget {
+    // 3.
+    let kind: String = "JourneyBook-Roadworks-NI"
+    var body: some WidgetConfiguration {
+        // 4.
+        StaticConfiguration(
+            kind: kind,
+            provider: TrafficIncidentsProvider()
+        ) { entry in
+            TrafficWatchWidgetView(entry: entry)
+        }
+        // 5.
+        .configurationDisplayName("Northern Ireland Roadworks")
+        // 6.
+        .description("Northern Ireland Roadworks traffic updates")
+        // 7.
+        .supportedFamilies([
+            .systemMedium,
+            .systemLarge,
+        ])
+    }
+}
