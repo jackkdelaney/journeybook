c13666ee8068aa8a2258ed9c3bb9321411d82516
Jack Delaney <jdelaney07@qub.ac.uk>
2025-04-12 00:42:42 +0100

    Basic Logic Now working

--- JourneyBook/Sources/Model/RunningJourney/LiveJourneyStepModel.swift
+++ JourneyBook/Sources/Model/RunningJourney/LiveJourneyStepModel.swift
@@ -5,16 +5,24 @@
 //  Created by Jack Delaney on 11/04/2025.
 //
 
+import ActivityKit
+import CommonCodeKit
 import Foundation
 import SharedPersistenceKit
 import SwiftData
 
+/*
+ @State var activty : Activity<StepAttributes>?
+
+ */
 @Observable
 class LiveJourneyStepModel {
     let modelContainer: ModelContainer
     let modelContext: ModelContext
 
     var journey: Journey
+
+    var activty: Activity<StepAttributes>?
 
     @MainActor
     init(journey: Journey) {
@@ -30,6 +38,11 @@ class LiveJourneyStepModel {
         !liveJourneysByID.contains(journey.id)
     }
 
+    var theLiveJourney: LiveJourney? {
+        fetchResources()
+            .first(where: { $0.journey?.id == journey.id })
+    }
+
     private var liveJourneysByID: [UUID] {
         fetchResources()
             .compactMap { $0.journey }
@@ -42,6 +55,7 @@ class LiveJourneyStepModel {
         let liveJourney = LiveJourney(journey: journey)
         endJourneys()
         add(liveJourney)
+        startLiveActivity()
     }
 
     func endJourneys() {
@@ -53,6 +67,45 @@ class LiveJourneyStepModel {
 
         } catch {
             fatalError(error.localizedDescription)
+        }
+        stop()
+    }
+
+    private func startLiveActivity() {
+        let attributes = StepAttributes()
+        let contentState = StepAttributes.Status(stepNumber: theLiveJourney?.stepNumber ?? -1, totalSteps: theLiveJourney?.stepsAmount ?? -2, description: theLiveJourney?.journey?.journeyDescription)
+        do {
+            let staleDate = Calendar.current.date(byAdding: .day, value: 1, to: Date())
+
+            let content = ActivityContent(state: contentState, staleDate: staleDate, relevanceScore: 0.65)
+
+            activty = try Activity<StepAttributes>.request(
+                attributes: attributes,
+                content: content
+            )
+
+            //                    let _ = try Activity<ActivityAttributesSample>.request(attributes: attributes, contentState: contentState)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    private func updateActivity() {
+        let updatedContentState = StepAttributes.Status(stepNumber: 1, totalSteps: 2, description: "SUPER HOWDY")
+
+        if let activty {
+            Task {
+                await activty.update(using: updatedContentState)
+            }
+        }
+    }
+
+    private func stop() {
+        Task {
+            for activity in Activity<StepAttributes>.activities {
+                await activity.end(activity.content, dismissalPolicy: .immediate)
+            }
+            activty = nil
         }
     }
 }
