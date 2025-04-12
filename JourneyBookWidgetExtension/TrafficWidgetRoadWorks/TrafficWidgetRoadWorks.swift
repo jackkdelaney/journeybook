ef65bf6636e23c2d4986dae7167c97a88c8e901d
Jack Delaney <jdelaney07@qub.ac.uk>
2025-04-12 23:14:22 +0100

    Tests and Fixes

--- JourneyBookWidgetExtension/TrafficWidgetRoadWorks/TrafficWidgetRoadWorks.swift
+++ JourneyBookWidgetExtension/TrafficWidgetRoadWorks/TrafficWidgetRoadWorks.swift
@@ -9,21 +9,17 @@ import SwiftUI
 import WidgetKit
 
 struct TrafficWidgetRoadWorks: Widget {
-    // 3.
     let kind: String = "JourneyBook-Roadworks-NI"
     var body: some WidgetConfiguration {
-        // 4.
+
         StaticConfiguration(
             kind: kind,
             provider: TrafficIncidentsProvider()
         ) { entry in
             TrafficWatchWidgetView(entry: entry)
         }
-        // 5.
         .configurationDisplayName("Northern Ireland Roadworks")
-        // 6.
         .description("Northern Ireland Roadworks traffic updates")
-        // 7.
         .supportedFamilies([
             .systemMedium,
             .systemLarge,
