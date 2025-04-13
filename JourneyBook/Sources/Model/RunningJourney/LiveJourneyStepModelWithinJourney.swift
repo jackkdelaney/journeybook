ebee65665eb553ed465dd3c80831b95d882ebca8
Jack Delaney <jdelaney07@qub.ac.uk>
2025-04-13 19:28:30 +0100

    Live Ativity Improvements

--- JourneyBook/Sources/Model/RunningJourney/LiveJourneyStepModelWithinJourney.swift
+++ JourneyBook/Sources/Model/RunningJourney/LiveJourneyStepModelWithinJourney.swift
@@ -48,7 +48,7 @@ class LiveJourneyStepModel {
     
     
     func updateActivity() {
-        let updatedContentState = StepAttributes.Status(stepNumber: 1, totalSteps: 2, description: "SUPER HOWDY")
+        let updatedContentState = StepAttributes.Status(stepNumber: 1, totalSteps: 2, description: "SUPER HOWDY", title: "Updated")
 
         if let activty {
             let staleDate = Calendar.current.date(byAdding: .day, value: 1, to: Date())
@@ -122,7 +122,7 @@ class LiveJourneyStepModelWithinJourney : LiveJourneyStepModel {
 
     private func startLiveActivity() {
         let attributes = StepAttributes()
-        let contentState = StepAttributes.Status(stepNumber: theLiveJourney?.stepNumber ?? -1, totalSteps: theLiveJourney?.stepsAmount ?? -2, description: theLiveJourney?.journey?.journeyDescription)
+        let contentState = StepAttributes.Status(stepNumber: theLiveJourney?.stepNumber ?? -1, totalSteps: theLiveJourney?.stepsAmount ?? -2, description: theLiveJourney?.journey?.journeyDescription, title: theLiveJourney?.journey?.journeyName ?? "Unkown")
         do {
             let staleDate = Calendar.current.date(byAdding: .day, value: 1, to: Date())
 
--- Shared/StepAttributes.swift
+++ Shared/StepAttributes.swift
@@ -6,6 +6,7 @@
 //
 
 import ActivityKit
+import Foundation
 
 // BASED OFF https://ashishkakkad.com/2022/09/how-to-setup-your-app-for-dynamic-island-ios-16-1/
 public struct StepAttributes: ActivityAttributes {
@@ -13,13 +14,22 @@ public struct StepAttributes: ActivityAttributes {
     public struct ContentState: Codable, Hashable {
         public var stepNumber: Int
         public var totalSteps: Int
+        public var timeStarted: Date
 
         public var description: String?
+        public var title: String
 
-        public init(stepNumber: Int, totalSteps: Int, description: String?) {
+        public init(
+            stepNumber: Int,
+            totalSteps: Int,
+            description: String?,
+            title: String
+        ) {
             self.stepNumber = stepNumber
             self.totalSteps = totalSteps
             self.description = description
+            timeStarted = .now
+            self.title = title
         }
     }
 
