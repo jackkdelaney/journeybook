c937229fae8dc514728a5c9845ba145aedfb23dc
Jack Delaney <jdelaney07@qub.ac.uk>
2025-04-13 17:46:11 +0100

    Widget Improvements

--- JourneyBookWidgetExtension/PossibleJourneys/JourneyWidgetView.swift
+++ JourneyBookWidgetExtension/PossibleJourneys/JourneyWidgetView.swift
@@ -5,38 +5,41 @@
 //  Created by Jack Delaney on 13/04/2025.
 //
 
-import SwiftUI
-import WidgetKit
 import SharedPersistenceKit
 import SwiftData
+import SwiftUI
+import WidgetKit
+import AppIntents
 
 struct JourneyWidgetView: View {
     var entry: JourneyProvider.Entry
 
-    @Query(sort: \Journey.dateCreated) var journeys : [Journey]
+    @Query(sort: \Journey.dateCreated) var journeys: [Journey]
     @Environment(\.widgetFamily) var family
-    
-//    var selectedJourney : Journey {
-//        
-//    }
-    
-    @ViewBuilder
-    var content : some View {
-        if let journey = journeys.randomElement() {
-            VStack(alignment: .leading) {
-                Label(journey.journeyName, systemImage: "point.topright.arrow.triangle.backward.to.point.bottomleft.filled.scurvepath")
-                    .font(.title3)
-                    .bold()
-                    .padding(.bottom, 8)
-                Text("\(journey.steps.count) Steps")
-                    .font(.caption)
-                Spacer()
-                if let journeyDescription = journey.journeyDescription {
-                    Text(journeyDescription)
-                        .font(.caption2)
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                }
+
+    var body: some View {
+        showedView
+            .containerBackground(for: .widget) {
+                Color.pink.opacity(0.86)
             }
+    }
+
+    @ViewBuilder
+    private var showedView: some View {
+        switch entry.type {
+        case .ordinary:
+            content
+        case .placeholder:
+            theView(for: Journey.sample())
+        case .snapshot:
+            theView(for: Journey.sampleNewYork())
+        }
+    }
+
+    @ViewBuilder
+    private var content: some View {
+        if let journey = journeys.randomElement() {
+            theView(for: journey)
         } else {
             VStack {
                 Label("No Journey's", systemImage: "point.topright.arrow.triangle.backward.to.point.bottomleft.filled.scurvepath")
@@ -49,15 +52,21 @@ struct JourneyWidgetView: View {
             }
         }
     }
-    
-    
-    @ViewBuilder var showedView : some View {
-        Text("")
-    }
-    var body: some View {
-        showedView
-        .containerBackground(for: .widget) {
-            Color.pink.opacity(0.86)
+
+    private func theView(for journey: Journey) -> some View {
+        VStack(alignment: .leading) {
+            Label(journey.journeyName, systemImage: "point.topright.arrow.triangle.backward.to.point.bottomleft.filled.scurvepath")
+                .font(.title3)
+                .bold()
+                .padding(.bottom, 8)
+            Text("\(journey.steps.count) \(journey.steps.count == 1 ? "Step" : "Steps")")
+                .font(.caption)
+            Spacer()
+            if let journeyDescription = journey.journeyDescription {
+                Text(journeyDescription)
+                    .font(.caption2)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
         }
     }
 }
