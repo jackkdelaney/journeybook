5e343a1af9f1f25f896eee169f50a2d72deca0f8
Jack Delaney <jdelaney07@qub.ac.uk>
2025-04-15 12:52:37 +0100

    Imrpvoements

--- JourneyBook/Sources/Screens/ResourceManager/ResourceSectionImageViewer.swift
+++ JourneyBook/Sources/Screens/ResourceManager/ResourceSectionImageViewer.swift
@@ -7,9 +7,19 @@
 
 import SwiftUI
 
-struct ResourceSectionImageViewer: View {
-    @State private var scale: CGFloat = 1.0
+//IMPROVED THE GESTURE WITH THE HACKING WITH SWIFT INFO https://www.hackingwithswift.com/quick-start/swiftui/how-to-handle-pinch-to-zoom-for-views
 
+
+struct ResourceSectionImageViewer: View {
+    @State private var currentZoom = 0.0 {
+        didSet {
+            if currentZoom > 2 {
+                currentZoom = 2
+            }
+        }
+    }
+    @State private var totalZoom = 1.0
+    
     let image: UIImage
 
     var body: some View {
@@ -18,14 +28,24 @@ struct ResourceSectionImageViewer: View {
             .aspectRatio(contentMode: .fit)
             .frame(height: 300)
             .frame(maxWidth: .infinity)
-            .scaleEffect(scale)
-            .gesture(
-                MagnificationGesture()
-                    .onChanged { value in
-                        self.scale = value.magnitude
-                    }
-            )
             .removeListRowPaddingInsets()
-            
+            .scaleEffect(currentZoom + totalZoom)
+                        .gesture(
+                            MagnifyGesture()
+                                .onChanged { value in
+                                    currentZoom = value.magnification - 1
+                                }
+                                .onEnded { value in
+                                    totalZoom += currentZoom
+                                    currentZoom = 0
+                                }
+                        )
+                        .accessibilityZoomAction { action in
+                            if action.direction == .zoomIn {
+                                totalZoom += 1
+                            } else {
+                                totalZoom -= 1
+                            }
+                        }
     }
 }
