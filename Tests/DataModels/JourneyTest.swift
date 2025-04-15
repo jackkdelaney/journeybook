35ff0538168824fb0bbf0d54e7d02882d0e7e131
Jack Delaney <jdelaney07@qub.ac.uk>
2025-04-15 09:12:24 +0100

    More Tests

--- Tests/DataModels/JourneyTest.swift
+++ Tests/DataModels/JourneyTest.swift
@@ -5,14 +5,14 @@
 //  Created by Jack Delaney on 12/04/2025.
 //
 
-import Testing
 import Foundation
+import Testing
 
 @testable import SharedPersistenceKit
 
 struct JourneyTests {
+    // MARK: Tests for Initialisation
 
-    //MARK: Tests for Initialisation
     @Test
     func testInitialisationWithJustNameAndDesc() {
         let journey = Journey(journeyName: "Test Journey", journeyDescription: "A test journey description")
@@ -21,29 +21,29 @@ struct JourneyTests {
         #expect(journey.id != UUID())
         #expect(journey.dateCreated.timeIntervalSinceNow < 1)
     }
-    
+
     @Test
     func testInitialisationWithJustNameAndDescAndUUID() {
         let id = UUID()
-        let journey = Journey(id : id, journeyName: "Test Journey", journeyDescription: "A test journey description")
+        let journey = Journey(id: id, journeyName: "Test Journey", journeyDescription: "A test journey description")
         #expect(journey.journeyName == "Test Journey")
         #expect(journey.journeyDescription == "A test journey description")
         #expect(journey.id == id)
         #expect(journey.dateCreated.timeIntervalSinceNow < 1)
     }
-    
+
     @Test
     func testInitialisationComplete() {
         let id = UUID()
         let date = Date.distantPast
-        
-        let journey = Journey(id : id, dateCreated: date, journeyName: "Test Journey", journeyDescription: "A test journey description")
+
+        let journey = Journey(id: id, dateCreated: date, journeyName: "Test Journey", journeyDescription: "A test journey description")
         #expect(journey.journeyName == "Test Journey")
         #expect(journey.journeyDescription == "A test journey description")
         #expect(journey.id == id)
         #expect(journey.dateCreated == date)
     }
-    
+
     @Test
     func testInitialisationWithJustName() {
         let journey = Journey(journeyName: "Test Journey")
@@ -53,31 +53,30 @@ struct JourneyTests {
         #expect(journey.dateCreated.timeIntervalSinceNow < 1)
     }
 
-
     @Test
     func testJourneyNameMutation() {
         let journey = Journey(journeyName: "Initial Name")
         journey.journeyName = "Updated Name"
         #expect(journey.journeyName == "Updated Name")
     }
-    
+
     @Test
     func testJourneyNameMutationWhenNotDeclaredAtStart() {
         let journey = Journey(journeyName: "Initial Name")
         journey.journeyDescription = "THIS IS A DESCRIPTION"
         #expect(journey.journeyDescription == "THIS IS A DESCRIPTION")
     }
-    
+
     @Test
     func testJourneyNameMutationWhenDeclaredAtStart() {
-        let journey = Journey(journeyName: "Initial Name",journeyDescription: "Howdy.....")
+        let journey = Journey(journeyName: "Initial Name", journeyDescription: "Howdy.....")
         journey.journeyDescription = "THIS IS A DESCRIPTION"
         #expect(journey.journeyDescription == "THIS IS A DESCRIPTION")
     }
-    
+
     @Test
     func testJourneyNameMutationWhenDeclaredAtStartThenSetToNil() {
-        let journey = Journey(journeyName: "Initial Name",journeyDescription: "Howdy.....")
+        let journey = Journey(journeyName: "Initial Name", journeyDescription: "Howdy.....")
         journey.journeyDescription = nil
         #expect(journey.journeyDescription == nil)
     }
--- Tests/DataModels/VisualResourceTest.swift
+++ Tests/DataModels/VisualResourceTest.swift
@@ -5,24 +5,21 @@
 //  Created by Jack Delaney on 12/04/2025.
 //
 
-import Testing
 import Foundation
+import Testing
 
 @testable import SharedPersistenceKit
 
-
 struct VisualResourceTests {
-    
     @Test
     func testInitialization() {
         let sampleData = Data([0x00, 0x01, 0x02])
         let resource = VisualResource(resourceData: sampleData, resourceType: .image, aidDescription: "Test Description")
-        
+
         #expect(resource.id != UUID())
         #expect(resource.resourceData == sampleData)
         #expect(resource.resourceType == .image)
         #expect(resource.aidDescription == "Test Description")
         // #expect(resource.steps.isEmpty)  MARK: RENABLE ONCE STEPS BROUGH BACK!
     }
-  
 }
