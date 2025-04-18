58c414cf928f335c78fdcdb6d568b786c5b9fd47
Jack Delaney <jdelaney07@qub.ac.uk>
2025-04-18 14:17:09 +0100

    Fixes

--- JourneyBook/Sources/JourneyBookApp.swift
+++ JourneyBook/Sources/JourneyBookApp.swift
@@ -7,7 +7,7 @@
 
 import SharedPersistenceKit
 
-// import PostHog
+import PostHog
 import SwiftData
 import SwiftUI
 import WidgetKit
@@ -28,15 +28,19 @@ struct JourneyBookApp: App {
                let window = windowScene.windows.first
             {
                 window.layer.speed = 100
+                
             }
         }
 
-//        let POSTHOG_API_KEY = "phc_y0C7c21kV4Hn3qxLqNWdQev3Bhk66TnapSxK9fxVbAY"
-//        // usually 'https://us.i.posthog.com' or 'https://eu.i.posthog.com'
-//        let POSTHOG_HOST = "https://us.i.posthog.com"
-//
-//        let config = PostHogConfig(apiKey: POSTHOG_API_KEY, host: POSTHOG_HOST)
-//        PostHogSDK.shared.setup(config)
+        let POSTHOG_API_KEY = "phc_y0C7c21kV4Hn3qxLqNWdQev3Bhk66TnapSxK9fxVbAY"
+        let POSTHOG_HOST = "https://us.i.posthog.com"
+
+        let config = PostHogConfig(apiKey: POSTHOG_API_KEY, host: POSTHOG_HOST)
+        
+
+        
+        
+        PostHogSDK.shared.setup(config)
     }
 
     var body: some Scene {
