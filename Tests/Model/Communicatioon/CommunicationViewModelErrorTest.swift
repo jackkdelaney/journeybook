0a8618e903e30b0abf16ce7b475a9cd2e7c2c25a
Jack Delaney <jdelaney07@qub.ac.uk>
2025-04-18 12:39:20 +0100

    More Tests

---
+++ Tests/Model/Communicatioon/CommunicationViewModelErrorTest.swift
@@ -0,0 +1,31 @@
+//
+//  CommunicationViewModelErrorTest.swift
+//  AppExtensionJBKit
+//
+//  Created by Jack Delaney on 18/04/2025.
+//
+
+import Testing
+import SharedPersistenceKit
+
+@testable import JourneyBook
+import SwiftUI
+
+struct CommunicationViewModelErrorTests {
+    @Test
+    func testInitialisationSetsPropertiesCorrectly() {
+        let modelA = CommunicationViewModelError.noTitleText
+        let modelB = CommunicationViewModelError.noTitleText
+        #expect(modelA.hashValue == modelB.hashValue)
+    }
+    
+    
+    @Test("Test Error Message Function",
+          arguments: [CommunicationViewModelError.noTitleText, CommunicationViewModelError.noPhoneNumber, CommunicationViewModelError.noEmailOrMessage,CommunicationViewModelError.noPhoneOrmessage])
+    func convertDegrees(error: CommunicationViewModelError) {
+        let expectations : [CommunicationViewModelError: String] = [CommunicationViewModelError.noTitleText :   "Please enter a title.", CommunicationViewModelError.noPhoneNumber : "Please enter a Phone Number", CommunicationViewModelError.noEmailOrMessage : "Please check you have entered a email and messsage.",CommunicationViewModelError.noPhoneOrmessage :      "Please check you have entered a phone number and messsage."]
+        #expect(error.errorMessage == expectations[error])
+    }
+    
+    
+}
