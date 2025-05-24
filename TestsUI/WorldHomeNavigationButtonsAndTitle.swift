6bf1dd22e73ef41f30498979a738fe97f86e6fad
Jack Delaney <jdelaney07@qub.ac.uk>
2025-05-24 20:43:58 +0100

    Fix

--- TestsUI/WorldHomeNavigationButtonsAndTitle.swift
+++ TestsUI/WorldHomeNavigationButtonsAndTitle.swift
@@ -53,12 +53,6 @@ final class WorldHomeNavigationButtonsAndTitle: XCTestCase {
         )
     }
 
-    func testBriefcaseOptionGliderButton() throws {
-        try testBriefcaseOption(
-            menuButtonText: "GliderProofConcept",
-            navigationText: "Bus Stops"
-        )
-    }
 
     func testBriefcaseOptionCommunicationButton() throws {
         try testBriefcaseOption(
