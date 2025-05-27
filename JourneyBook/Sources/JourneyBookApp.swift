2b1b6391ac9df66ed151562294812bbd47411414
Jack Delaney <jdelaney07@qub.ac.uk>
2025-05-27 16:38:27 +0100

    Readds file

---
+++ JourneyBook/Sources/JourneyBookApp.swift
@@ -0,0 +1,38 @@
+//
+//  JourneyBookApp.swift
+//  JourneyBook
+//
+//  Created by Jack Delaney on 16/10/2024.
+//
+
+import SharedPersistenceKit
+
+import SwiftData
+import SwiftUI
+import WidgetKit
+
+@main
+struct JourneyBookApp: App {
+    init() {
+        AppShortcuts.updateAppShortcutParameters()
+
+        WidgetCenter.shared.reloadAllTimelines()
+
+        // FOR UI TESTS ONLY
+        if ProcessInfo.processInfo.arguments.contains("UITests") {
+            UIView.setAnimationsEnabled(false)
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let window = windowScene.windows.first
+            {
+                window.layer.speed = 100
+            }
+        }
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            CoordinatorView()
+        }
+        .modelContainer(for: [VisualResource.self, Phrase.self, Journey.self,  JourneyStep.self, TransportRoute.self, Communication.self])
+    }
+}
